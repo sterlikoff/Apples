29565ba634c732fc002ef3cd5de8228d01044264
sterlikoff <sterlikoff@mail.ru>
2021-04-16 14:46:40 +0300

    config

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=apple',
+            'username' => 'mysql',
+            'password' => 'mysql',
             'charset' => 'utf8',
         ],
         'mailer' => [
