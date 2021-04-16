ae524c6df2e00b37032b1282f36f2e1ae173a390
sterlikoff <sterlikoff@mail.ru>
2021-04-16 14:59:26 +0300

    access rules

--- backend/controllers/AppleController.php
+++ backend/controllers/AppleController.php
@@ -7,6 +7,7 @@ use common\models\Apple;
 use Exception;
 use Yii;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -21,6 +22,15 @@ class AppleController extends Controller
    */
   public function behaviors() {
     return [
+      'access' => [
+        'class' => AccessControl::className(),
+        'rules' => [
+          [
+            'allow' => true,
+            'roles' => ['@'],
+          ],
+        ],
+      ],
       'verbs' => [
         'class' => VerbFilter::className(),
         'actions' => [
